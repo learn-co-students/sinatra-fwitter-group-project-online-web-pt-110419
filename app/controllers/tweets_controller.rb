50f54ce70a6b044cc511ec04faf29007ec83a222
github username <github email address>
2020-03-02 17:24:02 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,74 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        if !logged_in?
+            redirect '/login'
+        # end
+         elsif logged_in?
+            @tweets = Tweet.all
+            @user = current_user #User.find(session[:user_id])
+            erb :'tweets/index'
+          end
+    end
+
+    get '/tweets/new' do
+        if !logged_in?
+            redirect '/login'
+        end
+        erb :'tweets/new'
+    end
+
+    post '/tweets' do
+        user = current_user
+        if params[:content].empty?
+            redirect '/tweets/new' 
+        end  
+        @tweet = Tweet.create(content: params[:content], :user_id => user.id)
+        # binding.pry
+        redirect '/tweets'
+    end
+
+    get '/tweets/:id' do
+        if !logged_in?
+            redirect '/login'
+        end
+        @tweet = Tweet.find(params[:id]) 
+        # binding.pry
+        erb :'/tweets/show'
+    end
+
+    get '/tweets/:id/edit' do
+        if !logged_in?
+            redirect '/login'
+        end
+         @tweet = Tweet.find(params[:id])
+         if current_user.id != @tweet.user_id
+            # binding.pry
+             redirect '/tweets'
+         end
+        erb :'/tweets/edit'
+    end
+
+    patch '/tweets/:id' do
+        tweet = current_user.tweets.find_by(id: params[:id])
+        if !params[:content].empty?
+            tweet.update(content: params[:content])
+
+            redirect "/tweets/#{params[:id]}"
+        else        
+            redirect "/tweets/#{params[:id]}/edit"  
+        end 
+    end
+
+    post '/tweets/:id/delete' do
+        tweet = current_user.tweets.find_by(id: params[:id])
+        if tweet && tweet.destroy
+            redirect "/tweets"
+        else
+            redirect "/tweets/#{tweet.id}"
+    end
+
+    end
+
 
 end
