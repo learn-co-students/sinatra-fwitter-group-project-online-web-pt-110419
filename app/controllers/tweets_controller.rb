6258bf1e08080406d70ecda909e4d47d5989711f
Anthony Mai <anthony.mai530@gmail.com>
2020-03-05 02:35:17 +0000

    homepage

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,9 @@
-class TweetsController < ApplicationController
+require './config/environment'
 
+class TweetsController < ApplicationController
+  configure do
+    enable :sessions
+    set :session_secret, "tweet_secret"
+  end
 
 end
