c5640b7f4d1383ec597c115deeeae46c9b6e958b
Anthony Mai <anthony.mai530@gmail.com>
2020-03-11 01:00:27 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+  
+  def slug
+    @slug = slugify(self.username)
+  end
+  
+  def slugify(name)
+    split_apostrophes = name.split(/[']/)
+    slugged_name = split_apostrophes.join
+    name_array = slugged_name.downcase.split(/[\W]/)
+    name_spaces = name_array.delete_if{|x| x ==""}
+    slugified = name_spaces.join("-")
+  end
+  
+  def self.find_by_slug(slug)
+    self.all.detect{|x| x.slug == slug}
+  end
+    
 end
