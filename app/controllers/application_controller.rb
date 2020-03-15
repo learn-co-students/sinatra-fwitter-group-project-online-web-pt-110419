aa5b68aaf65e681f61e356f1eeb467d7f887b055
Aida Abdikulova <abdikulova@gmail.com>
2020-03-15 20:45:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,63 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'/users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] != "" && params[:email] != "" && params[:password] != ""
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    session[:user_id] = @user.id
+    redirect '/tweets'
+    else
+    redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+    else
+    erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/tweets'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
---
+++ app/models/helpers.rb
@@ -0,0 +1,7 @@
+class Helpers
+
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
+
+end
