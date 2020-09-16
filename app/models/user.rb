e39151a0733b4b066080363388dc51790e7d178d
Aida Abdikulova <aabdikul@Aidas-MacBook-Pro.local>
2020-09-16 11:45:37 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,6 @@ class User < ActiveRecord::Base
 		new_array
 	end
 
-  def self.find_by_slug(slug)
-    slugged = []
-    array = slug.split("-")
-    array.each do |word|
-    slugged << word
-    end
-    final = slugged.join(" ")
-    self.find_by("username like ?", final)
-  end
+ 
 
 end
